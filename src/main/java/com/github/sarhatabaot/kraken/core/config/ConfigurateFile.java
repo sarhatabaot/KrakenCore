11f4fbe62307ede97c1a6127f34c95cbf3d60c07
Omer Oreg <sarhatabaot1@gmail.com>
2022-06-20 11:30:42 +0300

    Abstracted configurate files

--- src/main/java/com/github/sarhatabaot/kraken/core/config/ConfigurateFile.java
+++ src/main/java/com/github/sarhatabaot/kraken/core/config/ConfigurateFile.java
@@ -4,44 +4,50 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.configurate.CommentedConfigurationNode;
 import org.spongepowered.configurate.ConfigurateException;
-import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
-
-import java.nio.file.Paths;
+import org.spongepowered.configurate.loader.AbstractConfigurationLoader;
 
 /**
  * @author sarhatabaot
+ * @param <T> JavaPlugin
+ * @param <U> Loader Type
+ * @param <R> Builder Type
  */
-public abstract class ConfigurateFile<T extends JavaPlugin> extends ConfigFile<T>{
-    protected final YamlConfigurationLoader.Builder loaderBuilder = YamlConfigurationLoader.builder().
-            path(Paths.get(folder+"/"+fileName));
-    protected final YamlConfigurationLoader loader;
+public abstract class ConfigurateFile<T extends JavaPlugin, U extends AbstractConfigurationLoader<CommentedConfigurationNode>,R extends AbstractConfigurationLoader.Builder<R, U>> extends ConfigFile<T> {
+    protected final U loader;
+    protected final R loaderBuilder;
+
     protected CommentedConfigurationNode rootNode;
     protected Transformation transformation;
 
-    protected ConfigurateFile(@NotNull final T plugin, final String resourcePath, final String fileName, final String folder)  throws ConfigurateException {
+    protected ConfigurateFile(@NotNull final T plugin, final String resourcePath, final String fileName, final String folder) throws ConfigurateException {
         super(plugin, resourcePath, fileName, folder);
-        preLoaderBuild();
+        this.loaderBuilder = loadBuilder();
+
+        builderOptions();
         this.loader = loaderBuilder.build();
         this.rootNode = loader.load();
 
         this.saveDefaultConfig();
-        initValues();
-        plugin.getLogger().info(() -> "Loading "+fileName);
-
         this.transformation = getTransformation();
-        if(this.transformation != null) {
-            this.transformation.updateNode(loader.load());
+        if (this.transformation != null) {
+            loader.save(this.transformation.updateNode(rootNode));
+            this.rootNode = loader.load();
         }
+
+        initValues();
+        plugin.getLogger().info(() -> "Loading " + fileName);
     }
 
     protected abstract void initValues() throws ConfigurateException;
 
-    protected abstract void preLoaderBuild();
+    protected abstract R loadBuilder();
+
+    protected abstract void builderOptions();
 
     protected abstract Transformation getTransformation();
 
     @Override
-    public void reloadConfig()  {
+    public void reloadConfig() {
         try {
             this.rootNode = loader.load();
             initValues();
