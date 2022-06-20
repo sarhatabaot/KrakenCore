1b2c097f2a7d8e2138dc8b6aab75e6122b9fc513
Omer Oreg <sarhatabaot1@gmail.com>
2022-06-20 14:39:07 +0300

    Fully abstracted the configuratefiles

--- src/main/java/com/github/sarhatabaot/kraken/core/config/HoconConfigurateFile.java
+++ src/main/java/com/github/sarhatabaot/kraken/core/config/HoconConfigurateFile.java
@@ -2,52 +2,23 @@ package com.github.sarhatabaot.kraken.core.config;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
-import org.spongepowered.configurate.CommentedConfigurationNode;
 import org.spongepowered.configurate.ConfigurateException;
 import org.spongepowered.configurate.hocon.HoconConfigurationLoader;
 
+import java.io.File;
 import java.nio.file.Paths;
 
 /**
  * @author sarhatabaot
  */
-public abstract class HoconConfigurateFile <T extends JavaPlugin> extends ConfigFile<T> {
-    protected final HoconConfigurationLoader.Builder loaderBuilder = HoconConfigurationLoader.builder().
-            path(Paths.get(folder+"/"+fileName));
-    protected final HoconConfigurationLoader loader;
-    protected CommentedConfigurationNode rootNode;
-    protected Transformation transformation;
-
+public abstract class HoconConfigurateFile <T extends JavaPlugin> extends ConfigurateFile<T, HoconConfigurationLoader, HoconConfigurationLoader.Builder> {
     protected HoconConfigurateFile(@NotNull final T plugin, final String resourcePath, final String fileName, final String folder)  throws ConfigurateException {
         super(plugin, resourcePath, fileName, folder);
-        builderOptions();
-        this.loader = loaderBuilder.build();
-        this.rootNode = loader.load();
-
-        this.saveDefaultConfig();
-        initValues();
-        plugin.getLogger().info(() -> "Loading "+fileName);
-
-        this.transformation = getTransformation();
-        if(this.transformation != null) {
-            this.loader.save(this.transformation.updateNode(rootNode));
-            this.rootNode = loader.load();
-        }
     }
 
-    protected abstract void initValues() throws ConfigurateException;
-
-    protected abstract void builderOptions();
-
-    protected abstract Transformation getTransformation();
-
     @Override
-    public void reloadConfig()  {
-        try {
-            this.rootNode = loader.load();
-            initValues();
-        } catch (ConfigurateException e) {
-            plugin.getLogger().severe(e.getMessage());
-        }
+    protected HoconConfigurationLoader.Builder loadBuilder() {
+        return HoconConfigurationLoader.builder().
+                path(Paths.get(folder+ File.separator+fileName));
     }
 }
---
+++ src/main/java/com/github/sarhatabaot/kraken/core/config/YamlConfigurateFile.java
@@ -0,0 +1,25 @@
+package com.github.sarhatabaot.kraken.core.config;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.configurate.ConfigurateException;
+import org.spongepowered.configurate.yaml.NodeStyle;
+import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+/**
+ * @author sarhatabaot
+ */
+public abstract class YamlConfigurateFile<T extends JavaPlugin> extends ConfigurateFile<T, YamlConfigurationLoader, YamlConfigurationLoader.Builder>{
+    public YamlConfigurateFile(@NotNull final T plugin, final String resourcePath, final String fileName, final String folder) throws ConfigurateException {
+        super(plugin, resourcePath, fileName, folder);
+    }
+
+    @Override
+    protected YamlConfigurationLoader.Builder loadBuilder() {
+        return YamlConfigurationLoader.builder()
+                .path(Paths.get(folder+ File.separator+fileName)).nodeStyle(NodeStyle.BLOCK);
+    }
+}
