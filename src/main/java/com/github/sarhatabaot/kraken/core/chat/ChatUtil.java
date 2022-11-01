25b3c99894d2d1421120b4828c8be2c82c4a7034
Omer Oreg <sarhatabaot1@gmail.com>
2022-11-01 07:31:59 +0200

    Added format for minimessage and placeholderapi

--- src/main/java/com/github/sarhatabaot/kraken/core/chat/ChatUtil.java
+++ src/main/java/com/github/sarhatabaot/kraken/core/chat/ChatUtil.java
@@ -1,7 +1,11 @@
 package com.github.sarhatabaot.kraken.core.chat;
 
+import me.clip.placeholderapi.PlaceholderAPI;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
@@ -22,5 +26,27 @@ public class ChatUtil {
     @Contract("_ -> new")
     public static @NotNull String color(final String string) {
         return ChatColor.translateAlternateColorCodes('&',string);
+    }
+
+    public static @NotNull String format(final Player player, final String text) {
+        String finalText = text;
+        if(PlaceholderAPI.containsPlaceholders(text)) {
+            finalText = PlaceholderAPI.setPlaceholders(player, text);
+        }
+
+        //MiniMessage
+        if(finalText.contains("<") || finalText.contains(">")) {
+            MiniMessage miniMessage = MiniMessage.miniMessage();
+            finalText = LegacyComponentSerializer.builder()
+                    .hexColors()
+                    .useUnusualXRepeatedCharacterHexFormat()
+                    .build().serialize(miniMessage.deserialize(finalText));
+        }
+
+        //LegacyFormatting
+        if(finalText.contains("&")) {
+            finalText = ChatUtil.color(finalText);
+        }
+        return finalText;
     }
 }
