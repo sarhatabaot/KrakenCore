a7aab11ea0ecd0942d6bbf01096062699fe58959
Omer Oreg <sarhatabaot1@gmail.com>
2023-04-06 13:48:07 +0300

    Moved to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,74 @@
+plugins {
+    id("java")
+    id("maven-publish")
+    id("com.github.johnrengelman.shadow") version "8.1.0"
+    
+    jacoco
+}
+
+version = "1.7.0"
+
+repositories {
+    maven(
+        url = "https://repo.aikar.co/content/groups/aikar/"
+    )
+    maven(
+        url = "https://mvn.lumine.io/repository/maven-snapshots/"
+    )
+    maven(
+        url = "https://repo.extendedclip.com/content/repositories/placeholderapi/"
+    )
+    maven (
+        url ="https://jitpack.io"
+    )
+}
+
+
+dependencies {
+    compileOnly(libs.spigot.api)
+    compileOnly(libs.placeholder.api)
+    
+//    compileOnly(libs.hikaricp)
+//    compileOnly(libs.flyway)
+    compileOnly(libs.annotations)
+    compileOnly(libs.adventure.api)
+    compileOnly(libs.adventure.minimessage)
+    compileOnly(libs.adventure.bukkit)
+    
+    compileOnly(libs.artifact.version)
+    compileOnly(libs.jooq)
+    
+    compileOnly(libs.slf4j)
+    
+    implementation(libs.lapzupi.config)
+    implementation(libs.lapzupi.connection)
+    
+    testImplementation(libs.mockito)
+    testImplementation(platform(libs.junit.platform))
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.jooq)
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+    
+    shadowJar {
+        minimize()
+        
+        relocate("com.lapzupi.dev.connection", "com.github.sarhatabaot.kraken.core.db")
+        relocate("com.lapzupi.dev.config", "com.github.sarhatabaot.kraken.core.config")
+    }
+}
+jacoco {
+    toolVersion = "0.8.8"
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,39 @@
+rootProject.name = "KrakenCore"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("annotations", "org.jetbrains:annotations:24.0.1")
+            library("spigot-api", "org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
+    
+            version("jooq", "3.17.8")
+            library("jooq", "org.jooq","jooq").versionRef("jooq")
+            library("jooq-codegen", "org.jooq", "jooq-codegen").versionRef("jooq")
+            library("jooq-meta", "org.jooq", "jooq-meta").versionRef("jooq")
+            library("jooq-meta-extensions","org.jooq","jooq-meta-extensions").versionRef("jooq")
+            
+            version("configurate", "4.1.2")
+            library("configurate-core", "org.spongepowered", "configurate-core").versionRef("configurate")
+            library("configurate-yaml", "org.spongepowered", "configurate-yaml").versionRef("configurate")
+            library("configurate-gson", "org.spongepowered", "configurate-gson").versionRef("configurate")
+            
+            version("adventure", "4.13.0")
+            library("adventure-api", "net.kyori","adventure-api").versionRef("adventure")
+            library("adventure-minimessage", "net.kyori", "adventure-text-minimessage").versionRef("adventure")
+            library("adventure-bukkit","net.kyori:adventure-platform-bukkit:4.3.0")
+            
+            library("placeholder-api","me.clip:placeholderapi:2.11.2")
+            
+            library("artifact-version", "org.apache.maven:maven-artifact:3.9.0")
+    
+            library("mockito", "org.mockito:mockito-core:5.1.1")
+            library("junit-platform","org.junit:junit-bom:5.9.2")
+            library("junit-jupiter", "org.junit.jupiter","junit-jupiter").withoutVersion()
+            
+            library("lapzupi-connection", "com.github.Lapzupi:LapzupiConnection:1.0.2")
+            library("lapzupi-config", "com.github.Lapzupi:LapzupiConfig:1.1.0")
+            
+            library("slf4j", "org.slf4j:slf4j-api:2.0.5")
+        }
+    }
+}
